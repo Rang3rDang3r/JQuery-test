eb813f92183b58205ab559c1cc943887c3dce682
Rang3rDang3r <stu-evans@live.co.uk>
2019-11-21 13:14:11 +0000

    another quize

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    $("#button1").addClass("makeRed")
+    .mouseenter.addClass("makeBorder").removeClass("makeRed")
+    .mouseleave.removeClass("makeBorder").addClass("makeRed");
+
+});
